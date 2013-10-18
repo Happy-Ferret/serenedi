aaccfb3b3117312474ca532044a3fe191b67de04
CoodingTwinky <twinky@codingtwinky.com>
2013-10-18 04:10:40 +0000

    Initial commit.... Long ways to go and far from good...
    Please enter the commit message for your changes. Lines starting

---
+++ public/js/serenedi.js
@@ -0,0 +1,516 @@
+var map;
+var markersArray = [];
+var ids = [];
+var lastClickMarker = null;
+var latestLat = null;  
+var latestLng = null; 
+var distCheckPass = null;
+var updateTime = 0;
+var eventToOpenID = null;  //Not
+var eventToOpen = null;
+var lastOpen;
+var lastClickMarker;
+
+var working = false;   
+var dragging = false;
+var needUpdate = true;
+var updateTimeCheck = true;
+var distanceCheck = true;
+var radiusCheck = true;
+var MAX_NUMBER = 9007199254740992;
+var socket;
+var clearMarkers = false;
+
+    function init() {
+        setupSocket();
+        windowResize();
+        $(window).resize(windowResize);
+
+        today = new Date();
+
+
+        eventToOpenID = getURLArgument.id;
+
+
+        $("#dateFrom").datepicker({
+            defaultDate : "",
+            changeMonth : true,
+            changeYear : true,
+            numberOfMonths : 1,
+            onSelect : function(selectedDate) {
+                $("#dateTo").datepicker("option", "minDate", selectedDate);
+                clearMarkers = true;
+                callUpdateMap(true);
+            }
+        });
+        $("#dateTo").datepicker({
+            defaultDate : "+1w",
+            changeMonth : true,
+            changeYear : true,
+            numberOfMonths : 1,
+            onSelect : function(selectedDate) {
+                $("#dateFrom").datepicker("option", "maxDate", selectedDate);
+                clearMarkers = true;
+                callUpdateMap(true);
+            }
+        });
+
+        today = new Date();
+        $("#dateFrom").val(getPrettyDate(today));
+        $("#dateTo").datepicker("option", "minDate", today);
+        todayPlusOne = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7);
+        $("#dateTo").val(getPrettyDate(todayPlusOne));
+        $("#dateFrom").datepicker("option", "maxDate", todayPlusOne);
+
+      
+        $("#menuBox").mCustomScrollbar();
+        typeChanged();
+        $(".type").change(typeChanged);
+        clearMarkers = false;
+
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                lat = Math.round(position.coords.latitude * 100000) / 100000;
+                lng = Math.round(position.coords.longitude * 100000) / 100000;
+
+                $('#lat').val(lat);
+                $('#lng').val(lng);
+
+                initializeMap(lat, lng);
+            }, function err() {
+                initializeMap(39.8282, -98.5795);
+            });
+        } else {
+            initializeMap(39.8282, -98.5795);
+        }
+    }
+
+    function initializeMap(lat, lng) {
+        var point = new google.maps.LatLng(lat, lng);
+
+
+        map = new google.maps.Map(document.getElementById('mapBox'), {
+            zoom : 15,
+            center : point,
+            mapTypeId : google.maps.MapTypeId.ROADMAP
+        });
+
+        google.maps.event.addListenerOnce(map, 'idle', function() {
+            ne = map.getBounds().getNorthEast();
+            sw = map.getBounds().getSouthWest();
+
+            $('#radius').val(getDistanceFromLatLng(ne.lat(), ne.lng(), sw.lat(), sw.lng()) / 4);
+            $('#lat').val(Math.round(map.getCenter().lat() * 100000) / 100000);
+            $('#lng').val(Math.round(map.getCenter().lng() * 100000) / 100000);
+
+            updateMap();
+        });
+
+        google.maps.event.addListener(map, 'dragstart', function() {
+            dragging = true;
+        });
+
+        google.maps.event.addListener(map, 'dragend', function() {
+            $('#lat').val(Math.round(map.getCenter().lat() * 100000) / 100000);
+            $('#lng').val(Math.round(map.getCenter().lng() * 100000) / 100000);
+            dragging = false;
+            callUpdateMap(false);
+        });
+
+        google.maps.event.addListener(map, 'zoom_changed', function() {
+            
+            ne = map.getBounds().getNorthEast();
+            sw = map.getBounds().getSouthWest();
+
+            $('#radius').val(getDistanceFromLatLng(ne.lat(), ne.lng(), sw.lat(), sw.lng()));
+
+            callUpdateMap(true);
+        });
+    }
+
+    function updateMap() {
+        
+        updateTimeCheck = (new Date().getTime() - updateTime) > 800;
+        distanceCheck = (distCheckPass || ((latestLat == null && latestLng == null) || Math
+			.abs(getDistanceFromLatLng($('#lat').val(), $('#lng').val(),
+					latestLat, latestLng)) > $('#radius').val() / 1.5));
+        radiusCheck = $('#radius').val() < 20;
+
+        if(needUpdate && !dragging && !working && updateTimeCheck && distanceCheck && radiusCheck) {
+            showWorking();
+            needUpdate = false;
+            latestLat = $('#lat').val();
+            latestLng = $('#lng').val();
+            dragging = false;
+            working = false;
+            
+            if (eventToOpenID == null || eventToOpenID == 'undefined') {
+                socket.emit('getEventsCall', {
+                    message: { lat : $('#lat').val(),
+                               lng : $('#lng').val(),
+                               dateFrom : $('#dateFrom').val(),
+                               dateTo : $('#dateTo').val(),
+                               type : $('#categories').val(),
+                               radius : $('#radius').val() }
+                });
+            } else {
+                socket.emit('getEventsByIDCall', {
+                    message: { id : eventToOpenID,
+                               radius : $('#radius').val()}
+                });
+            }
+         }
+
+         if(!radiusCheck) {
+             showRadiusCheckFail();
+         }
+
+         setTimeout("updateMap()", 1000);
+    }
+
+    function setupSocket() {
+        socket = io.connect('http://serenedi.com/');
+
+        socket.on('getEventsResult', function(data) {
+            m = 1;  
+            n = 0;
+console.log(data);
+            if(clearMarkers == true) {
+                clearOverlays();
+            }
+   
+            if(data.message != null) {
+
+                if(typeof(data.center) != 'undefined' || data.center != null){
+                    //console.log(data.center.lat + "/" + data.center.lng);
+                    map.setCenter(new google.maps.LatLng(data.center.lat, data.center.lng));
+                }
+
+                if(typeof(data.date) != 'undefined' || data.date != null) {
+                    $("#dateFrom").datepicker("option", "maxDate", data.date.endDate);
+                    $('#dateFrom').val(data.date.startDate);
+                    $("#dateTo").datepicker("option", "minDate", data.date.startDate);
+                    $('#dateTo').val(data.date.endDate);
+                }
+
+                while(m < data.message.events.length) {
+
+                    if(n >= ids.length) {
+                        ids[n] = MAX_NUMBER;
+                    }
+
+                    if(data.message.events[m].event.id < ids[n]) {
+                        //console.log(data.message.events[m].event.id);
+                        if(ids[n] == MAX_NUMBER) {
+                            ids.pop;
+                        }
+
+                        //console.log(data.message.events[m].event.id);
+                        ids.push(data.message.events[m].event.id);
+                        addMarkers(data.message.events[m].event);
+
+                        m++;
+                    } else if (data.message.events[m].event.id > ids[n]) { 
+                        n++;
+                    } else {
+                        n++;
+                        m++;
+                    }
+                }
+
+                ids.sort();
+                hideWorking();
+            } else {
+               hideWorking();
+               //console.log("No events");
+               showNoEvents();
+            }
+
+            if (eventToOpen != null) {
+                google.maps.event.trigger(eventToOpen, 'click');
+                eventToOpen = null;
+            }
+            eventToOpenID = null;
+        });
+    }
+
+    function addMarkers(event) {
+        var point = new google.maps.LatLng(event.venue.latitude, event.venue.longitude);
+       
+        var marker = new google.maps.Marker({
+            position: point,
+            map : map,
+            title : event.title,
+            animation : google.maps.Animation.DROP,
+            clickable : true
+        });
+
+       marker.info = new google.maps.InfoWindow({content: '<strong>' + event.title + '</strong><br />'});
+       markersArray.push(marker);
+
+       if (event.id == eventToOpenID) {
+           eventToOpen = marker;
+       }
+
+       google.maps.event.addListener(
+           marker,
+           'click',
+           function() {
+               if (lastOpen != null) {
+                   lastOpen.close();
+               }
+               if (lastClickMarker != null) {
+                   lastClickMarker.setAnimation(null);
+               }
+               
+		//TODO USE CSS FOR THE LOVE OF MOTHER TERESA
+               var content = '<div id="infoTable" class="" style="font-size:10pt; font-family: Helvetica; width: 440px; height: 500px; overflow:hidden;">';
+               content += '<table width="100%" height="100%" cellpadding="0" cellspacing="0" style="overflow-x:hidden; padding-left: 5px; margin-left: 15px;" >';
+               content += '<tr><td colspan="2"><strong>' + marker.getTitle() + '</strong></td><br />';
+               content += '<tr><td width="75px">URL: </td><td><a href="' + event.url	+ '" target="_blank">' + event.url + '</a><br /><a target="_blank" href=http://www.serenedi.com/?id=' + event.id + '>http://www.serenedi.com/?id=' + event.id + '</a>' + '</td></tr>';
+               content += '<tr><td>Start: </td><td>' + event.start_date.split(' ')[0] + '</td></tr>';
+               if (event.end_date != null) {
+                   content += '<tr><td>End: </td><td>' + event.end_date.split(' ')[0] + '</td></tr>';
+               }
+               if (event.venue.address != null || event.venue.address != '') {
+                   content += '<tr><td>Address: </td><td>' + event.venue.address + ' ' + event.venue.address_2 + '</td></tr>';
+                   content += '<tr><td></td><td>' + event.venue.city + ', ' + event.venue.region + ' ' + event.venue.postalcode + '</td></tr>';
+               }
+               content += '<tr><td>Category: </td><td>' + event.category + '</td></tr>';
+               content += '<tr><td colspan="2" align="left"><div class="fb-like" data-href="http://www.serenedi.com/?id=' + event.id + '" data-send="true" data-layout="button_count" data-width="350" data-show-faces="true"></div></td></tr>';
+               content += '<tr><td colspan="2"><div> <script src="http://connect.facebook.net/en_US/all.js#appId=5006939796&amp;xfbml=1"></script><fb:comments href="www.serenedi.com/?id=' + event.id + '" num_posts="2" width="400"> </script></div></td></tr>';
+               content += '</table></div>';
+
+               var info = new google.maps.InfoWindow();
+               info.setContent(content);
+
+               google.maps.event.addListenerOnce(info, 'closeclick', function() {
+                   marker.setAnimation(null);
+               });
+
+               info.open(map, marker);
+
+               marker.setAnimation(google.maps.Animation.BOUNCE);
+
+               lastOpenInfo = info;
+               lastClickMarker = marker;
+
+               setTimeout(function() {
+                   try {
+                       FB.XFBML.parse();
+                   } catch (ex) {
+                   }
+               } , 100);
+           }
+       );
+
+    }
+
+
+    function callUpdateMap(flag) {
+        distCheckPass = flag;
+        updateTime = new Date().getTime();
+        needUpdate = true;
+    }
+
+	//TODO There has to be a better way then resizing like this... 
+    function windowResize() {
+        $('#1andOnlyAd').css('right', (($(window).width() - 468 - 337) / 2));
+ 
+        $("#mapBox").width(($(window).width() - 371) + "px");
+        $("#menuBox").height(($(window).height() - 60) + "px");
+        $("#mapBox").height(($(window).height() - 49) + "px");
+        $("body").height(($(window).height() - 45) + "px");
+    }
+
+function getPrettyDate(date) {
+        var month = date.getMonth() + 1;
+        var day = date.getDate();
+
+        if (month < 10) {
+                month += "0";
+        } 
+        if (day < 10) {
+                day += "0";
+        } 
+
+        return month + '/' + day + '/' + date.getFullYear();
+}   
+
+
+    function getDistanceFromLatLng(lat1, lng1, lat2, lng2) {
+        var R = 6371;   // Radius of the earth in KM
+        var dLat = deg2rad(lat2 - lat1);
+        var dLng = deg2rad(lng2 - lng1);
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLng / 2) * Math.sin(dLng / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return  R * c;
+    }
+
+    function deg2rad(deg) { 
+        return deg * (Math.PI / 180);
+    }
+
+
+//TODO again...  need better way of handling....
+    function typeChanged() {
+        clearMarkers = true;
+
+	result = "";
+
+	if ($('#typeConfFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+	if ($('#typeConvFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+	if ($('#typeEntFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+	if ($('#typeFairFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+	if ($('#typeFoodFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+	if ($('#typeFundFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+	if ($('#typeMeetFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+	if ($('#typeMusicFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+	if ($('#typePerfFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+	if ($('#typeRecFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+	if ($('#typeReligFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+	if ($('#typeReunFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+	if ($('#typeSalesFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+	if ($('#typeSemiFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+	if ($('#typeSociFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+	if ($('#typeSportsFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+	if ($('#typeTradeFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+	if ($('#typeTravelFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+	if ($('#typeOtherFlag').prop('checked')) {
+		result += "1";
+	} else {
+		result += "0";
+	}
+
+	$('#categories').val(result);
+        callUpdateMap(true);
+    }
+
+    function clearOverlays() {
+        for(i = 0; i < markersArray.length; i++) {
+            markersArray[i].setMap(null);
+        }
+        markersArray = [];
+        ids = [];
+        clearMarkers = false;
+    }
+
+    function showWorking() {
+        if ($('#working').html().indexOf('Working') == -1) {
+            $('#working').html("<img title='Working on loading more events...' src='../images/ajax-loader.gif' width='20px'/>");
+            $('#working').tooltip();
+        }
+    }
+
+    function showRadiusCheckFail() {
+        if ($('#working').html().indexOf('zoom') == -1) {
+            $('#working').html("<img title='Please zoom in to load events...' src='../images/warning.png' />");
+            $('#working').tooltip();
+        }
+    }
+
+    function showNoEvents() {
+        if ($('#working').html().indexOf('events') == -1) {
+            $('#working').html("<img title='There are no events with given criterias...' src='../images/warning.png' />");
+            $('#working').tooltip();
+        }
+    }
+
+    function hideWorking() {
+        $('#working').html("");
+    }
+
+    var getURLArgument = function () {
+        // This function is anonymous, is executed immediately and 
+        // the return value is assigned to QueryString!
+        var query_string = {};
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            // If first entry with this name
+            if (typeof query_string[pair[0]] === "undefined") {
+                query_string[pair[0]] = pair[1];
+                // If second entry with this name
+            } else if (typeof query_string[pair[0]] === "string") {
+                var arr = [ query_string[pair[0]], pair[1] ];
+                query_string[pair[0]] = arr;
+    	        // If third or later entry with this name
+            } else {
+                query_string[pair[0]].push(pair[1]);
+            }
+        } 
+        return query_string;
+    } ();
---
+++ serenedi.js
@@ -0,0 +1,197 @@
+var EVENTBRITE_API_KEY = 'GZEMIXVES3GD4IPRJX';
+var READ_SIZE          = 100;
+
+
+var express = require( 'express' );
+var app     = express();
+var poet    = require( 'poet' )( app );
+var fs      = require( 'fs' );
+var server  = require( 'http' ).createServer( app );
+var io      = require( 'socket.io' ).listen( app.listen( 3080 ) );
+var eventbrite = require( 'eventbrite' );
+var eb_client = eventbrite( {'app_key' : EVENTBRITE_API_KEY} );
+
+poet
+  .createPostRoute()
+  .createPageRoute()
+  .createTagRoute()
+  .createCategoryRoute()
+  .init();
+
+app.set( 'view engine', 'jade' );
+app.set( 'views', __dirname + '/views' );
+app.use( express.static( __dirname + '/public' ));
+
+
+app.get( '/', function ( req, res ) { res.render( 'index' ) });
+app.get( '/about', function ( req, res ) { res.render( 'about' ) });
+
+
+io.sockets.on('connection', function( socket ) {
+	socket.on('getEventsCall', function( data ) {
+        	params = {'longitude': data.message.lng,
+			'latitude' : data.message.lat,
+			'within_unit' : 'K',
+			'max' : READ_SIZE,
+			'page' : 1,
+			'within' : Math.ceil(data.message.radius),
+			'date' : getEventbriteDateRange(data.message.dateFrom, data.message.dateTo),
+			'category' : getTypeString(data.message.type),
+			'sort_by' : 'id'};
+		console.log(params);
+
+		eb_client.event_search(params, function(err, data){
+			socket.emit('getEventsResult', {message : data});
+		});
+	});
+
+	socket.on('getEventsByIDCall', function( data ) {
+		var params = {'id' : data.message.id};
+		var radius = Math.ceil(data.message.radius);
+		console.log(params);
+
+		eb_client.event_get(params, function(err, data){
+
+			var lat = data.event.venue.latitude;
+			var lng = data.event.venue.longitude;
+
+			var startDate = data.event.start_date.split(' ')[0].split('-');
+			var endDate = new Date(startDate);
+
+			startDate = startDate[1] + "/" + startDate[2] + "/" + startDate[0];
+			endDate.setDate(endDate.getDate() + 7);
+
+			endDateMonth = endDate.getMonth() + 1;
+			if (endDateMonth < 10) {
+				endDateMonth = '0' + endDateMonth;
+			}
+ 
+			endDateDay = endDate.getDate();
+			if (endDateDay < 10) {
+				endDateDay = '0' + endDateDay;
+			}
+
+			endDate = endDateMonth + "/" + endDateDay + "/" + endDate.getFullYear();
+
+			params = {'longitude': lng,
+				'latitude' : lat, 
+				'within_unit' : 'K', 
+				'date' : '', 
+				'max' : READ_SIZE, 
+				'page' : 1, 
+				'within' : 8, 
+				'sort_by' : 'id', 
+				'data' : getEventbriteDateRange(startDate, endDate), 
+				'within' : radius};
+
+			eb_client.event_search(params, function(err, data){
+
+				socket.emit('getEventsResult', {message: data, center:{lat: lat, lng: lng}, date:{startDate : startDate, endDate : endDate}});
+			});
+		});
+	});
+
+});
+
+function getPrettyDate(date) {
+	var month = date.getMonth() + 1;
+	var day = date.getDate();
+
+	if (month < 10) {
+		month += "0";
+	} 
+	if (day < 10) {
+		day += "0";
+	} 
+
+	return month + '/' + day + '/' + date.getFullYear();
+}
+
+
+
+function getEventbriteDateRange(from, to) {
+    fromArray = from.split('/');
+    toArray = to.split('/');
+
+    return fromArray[2] + '-' + fromArray[0] + '-' + fromArray[1] + ' ' + toArray[2] + '-' + toArray[0] + '-' + toArray[1];
+}
+
+//TODO There is a better way of handling this...  
+function getTypeString(type) {
+    typeStr = '';
+    
+    if(type.charAt(0) == '1') {
+        typeStr += 'conference, ';
+    }
+
+    if(type.charAt(1) == '1') {
+        typeStr += 'conventions, ';
+    }
+
+    if(type.charAt(2) == '1') {
+        typeStr += 'entertainment, ';
+    }
+
+    if(type.charAt(3) == '1') {
+        typeStr += 'fairs, ';
+    }
+
+    if(type.charAt(4) == '1') {
+        typeStr += 'food, ';
+    }
+
+    if(type.charAt(5) == '1') {
+        typeStr += 'fundraisers, ';
+    }
+
+    if(type.charAt(6) == '1') {
+        typeStr += 'meetings, ';
+    }
+
+    if(type.charAt(7) == '1') {
+        typeStr += 'music, ';
+    }
+
+    if(type.charAt(8) == '1') {
+        typeStr += 'performances, ';
+    }
+
+    if(type.charAt(9) == '1') {
+        typeStr += 'recreation, ';
+    }
+
+    if(type.charAt(10) == '1') {
+        typeStr += 'religion, ';
+    }
+
+    if(type.charAt(11) == '1') {
+        typeStr += 'reunions, ';
+    }
+
+    if(type.charAt(12) == '1') {
+        typeStr += 'sales, ';
+    }
+
+    if(type.charAt(13) == '1') {
+        typeStr += 'social, ';
+    }
+
+    if(type.charAt(14) == '1') {
+        typeStr += 'sports, ';
+    }
+
+    if(type.charAt(15) == '1') {
+        typeStr += 'tradeshows, ';
+    }
+
+    if(type.charAt(16) == '1') {
+        typeStr += 'travel, ';
+    }
+
+    if(type.charAt(17) == '1') {
+        typeStr += 'other';
+    }
+
+    return typeStr;
+}
+
