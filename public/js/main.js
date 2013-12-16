192369dd56bd33c27a3f99204129fd3615c160b6
CoodingTwinky <twinky@codingtwinky.com>
2013-12-16 05:46:25 +0000

    Using bootstrap menu bar

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,6 @@ require('../../bower_components/jquery-ui/ui/jquery-ui.js');
 require('../../bower_components/canjs/can.jquery.js');
 require('../../bower_components/canjs/can.object.js');
 require('../../bower_components/canjs/can.control.plugin.js');
-require('../../bower_components/jquery-mousewheel/jquery.mousewheel.js');
 
 var menu = require('./source/MenuControl.js');
 var map = require('./source/MapControl.js');
@@ -24,19 +23,4 @@ $(document).ready(function() {
     new menu.MenuControl('#menuContainer');
     new map.MapControl('#main');
     $('#statusImg').tooltip();
-
-    windowResize();
-    $(window).resize(windowResize);
 });
-
-
-//TODO There has to be a better way then resizing like this... 
-function windowResize() {
-    $('#1andOnlyAd').css('right', (($(window).width() - 468 - 337) / 2));
-
-    $("#mapBox").width(($(window).width() - 371) + "px");
-    $("#menuBox").height(($(window).height() - 60) + "px");
-    $("#mapBox").height(($(window).height() - 49) + "px");
-    $("body").height(($(window).height() - 45) + "px");
-}
-
