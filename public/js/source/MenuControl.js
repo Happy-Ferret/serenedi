38a1aef889f7001d6d8edce60d29243efc469041
CoodingTwinky <twinky@codingtwinky.com>
2013-12-17 06:09:13 +0000

    Using bootstrap popups instead of jquery tooltips

--- public/js/source/MenuControl.js
+++ public/js/source/MenuControl.js
@@ -1,15 +1,16 @@
 var $ = require('../../../bower_components/jquery/jquery.min.js');
 
 var MenuControl = can.Control({
-    init: function(element, options) {
-        this.element.html(can.view('menuTemplate'), {});
-        $('#about').hide();
-    },
-    '#homeLink click': function(el, ev) {
-        $( "#aboutDialog" ).dialog( "close" );
-    },
-    '#aboutLink click': function(el, ev) {
-        $( "#aboutDialog" ).dialog( "open" );
-    }
+  init: function(element, options) {
+    this.element.html(can.view('menuTemplate'), {});
+    $('#about').hide();
+    $("#statusImg").popover();
+  },
+  '#homeLink click': function(el, ev) {
+    $( "#aboutDialog" ).dialog( "close" );
+  },
+  '#aboutLink click': function(el, ev) {
+    $( "#aboutDialog" ).dialog( "open" );
+  }
 });
 exports.MenuControl = MenuControl;
--- public/js/source/StatusObservable.js
+++ public/js/source/StatusObservable.js
@@ -1,4 +1,3 @@
-
 var $ = require('../../../bower_components/jquery/jquery.min.js');
 
 var status = new can.Observe({value: 0});
@@ -9,34 +8,29 @@ exports.status = status;
 // 2 no events
 // 3 radius check fail
 status.bind('change', function(event, attr, how, newVal, oldVal) {
-    console.log(newVal);
-    if (newVal == 0) {
-    	showNormal();
-    } else if (newVal == 1) {
-    	showWorking();
-    } else if (newVal == 2) {
-    	showNoEvents();
-    } else if (newVal == 3) {
-    	showZoomCheckFail();
-    }
+  if (newVal == 0) {
+    showNormal();
+  } else if (newVal == 1) {
+    showWorking();
+  } else if (newVal == 2) {
+    showNoEvents();
+  } else if (newVal == 3) {
+    showZoomCheckFail();
+  }
 });
 
 var showWorking = function () {
-    $('#statusImg').attr('title', 'Working...')
-        .attr('src', '/images/ajax-loader.gif');
+  $('#statusImg').attr({'data-content': 'Working...', src: '/images/ajax-loader.gif'});
 }
 
 var showZoomCheckFail = function () {
-    $('#statusImg').attr('title', 'Zoom level is too high. Please zoom in to load events.')
-        .attr('src', '/images/warning.png');
+  $('#statusImg').attr({'data-content': 'Zoom level is too high. Please zoom in to load events.', src: '/images/warning.png'});
 }
 
 var showNoEvents = function () {
-    $('#statusImg').attr('title', 'There are no events with given criterias.')
-        .attr('src', '/images/warning.png');
+  $('#statusImg').attr({'data-content': 'There are no events with given criterias.', src: '/images/warning.png'});
 }
 
 var showNormal = function () {
-    $('#statusImg').attr('title', 'There are no events with given criterias.')
-        .attr('src', '/images/serenedi3.ico');
+  $('#statusImg').attr({'data-content': 'Welcome to Serenedi!', src: '/images/serenedi3.ico'});
 }
