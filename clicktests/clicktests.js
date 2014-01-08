9100cb92469133cc4c9a73745d0a62212966e1f9
CodingTwinky <twinky@codingtwinky.com>
2014-01-07 20:40:52 -0600

    Adding client side click test with phantom!
    This thing is cewl! thanks to Fredrik Noren and his Ungit example.

---
+++ clicktests/clicktests.js
@@ -0,0 +1,46 @@
+
+var expect = require('expect.js');
+var helpers = require('./helpers');
+var server;
+
+var backgroundAction = function(method, url, callback) {
+  var tempPage = helpers.createPage(function(err) {
+    console.error('Caught error');
+    phantom.exit(1);
+  });
+  tempPage.open(url, method, function(status) {
+    if (status == 'fail') return callback({ status: status, content: tempPage.plainText });
+    tempPage.close();
+    callback();
+  });
+};
+
+
+var page = helpers.createPage(function(err) {
+  console.error('Caught error');
+  phantom.exit(1);
+});
+
+var test = helpers.test;
+
+test('Init', function(done) {
+  server = helpers.startSerenediServer(done);
+});
+
+test('Open home screen', function(done) {
+  page.open('http://localhost:' + helpers.config.port, function() {
+    helpers.waitForElement(page, '#menuContainer', function() {
+      done();
+    });
+  });
+});
+
+test('Stop the server', function(done) {
+  console.log(server.pid);
+  server.kill('SIGINT');
+  phantom.exit(0);
+  done();
+});
+
+helpers.runTests(page);
+
