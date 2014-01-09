d4aea697d6258cd63e6f24bea6cfc969662e8940
CodingTwinky <twinky@codingtwinky.com>
2014-01-08 22:04:56 -0600

    Added serenedi executable's arguments

--- source/Config.js
+++ source/Config.js
@@ -1,5 +1,7 @@
 var fs = require("fs");
-var argv = require("optimist").argv;
+var optimist = require("optimist");
+var path = require("path");
+var argv = optimist.argv;
 
 var config = {
   testTimeout: 10000,
@@ -7,7 +9,7 @@ var config = {
   viewportSize: { width: 800, height: 1600 }
 };
 if (!argv || !argv.port || !argv.eventbriteAPIkey || !argv.url) {
-  var configFile = fs.readFileSync(".serenedirc", "utf-8");
+  var configFile = fs.readFileSync(path.join(__dirname + "/../.serenedirc"), "utf-8");
   if (!configFile) {
     throw new Error("Missing .serenedirc");
   }
@@ -22,3 +24,14 @@ config.launch = argv.launch === undefined ? true : argv.launch;
 config.googleAPIKey = argv.googleAPIKey || configFile.googleAPIKey;
 config.url = argv.url || configFile.url;
 exports.config = config;
+
+if (argv.help) {
+  optimist.usage('Launch Serenedi: Location based event search web application.')
+    .describe('port', 'Serenedi\'s port number')
+    .describe('url', 'URL where Serenedi is hosted on. (includes port number, ex. [--url=127.0.0.1:80])')
+    .describe('launch', 'to launch Serenedi on start or not.')
+    .describe('googleAPIKey', 'Google API key');
+
+  optimist.showHelp();
+  process.exit(0);
+}
