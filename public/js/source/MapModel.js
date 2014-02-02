72f5a4444ecff63bf23c7f91675c511bcecd6ab2
CodingTwinky <twinky@codingtwinky.com>
2014-02-02 15:21:42 -0600

    Using function literal to create mapmodel

--- public/js/source/MapModel.js
+++ public/js/source/MapModel.js
@@ -1,25 +1,26 @@
 var $ = require("../../../bower_components/jquery/jquery.min.js");
 var util = require("./Util.js");
 
-var mapModel = {
-  map: null,
-  ids: [],
-  markers: [],
-  lastClick: {marker: null, info: null},
-  latestLoc: {lat: null, lng: null},
-  eventToOpenID: null,
-  dragging: false,
-  socket: null,
-  defaultLoc: {lat: 40.72616, lng: -73.99973},
-  waitedSinceLastChange: undefined
-};
-exports.mapModel = mapModel;
+var MapModel = function() {
 
-mapModel.centerToLatLng = function() {
+  this.map = null;
+  this.ids = [];
+  this.markers = [];
+  this.lastClick = {marker: null, info: null};
+  this.latestLoc = {lat: null, lng: null};
+  this.eventToOpenID = null;
+  this.dragging = false;
+  this.socket = null;
+  this.defaultLoc = {lat: 40.72616, lng: -73.99973};
+  this.waitedSinceLastChange = undefined;
+};
+exports.mapModel = new MapModel();
+
+MapModel.prototype.centerToLatLng = function() {
   this.map.setCenter(new google.maps.LatLng($("#lat").val(), $("#lng").val()));
 };
 
-mapModel.clearMap = function () {
+MapModel.prototype.clearMap = function () {
   this.closeLastOpen();
 
   for (var n = 0; n < this.markers.length; n++) {
@@ -30,11 +31,11 @@ mapModel.clearMap = function () {
   this.ids = [];
 };
 
-mapModel.getScreenTravelDistance = function() {
+MapModel.prototype.getScreenTravelDistance = function() {
   return util.getDistanceFromLatLng($("#lat").val(), $("#lng").val(), this.latestLoc.lat, this.latestLoc.lng);
 };
 
-mapModel.closeLastOpen = function () {
+MapModel.prototype.closeLastOpen = function () {
   if (this.lastClick.info) {
     this.lastClick.info.close();
   }
@@ -45,6 +46,6 @@ mapModel.closeLastOpen = function () {
   this.lastClick.marker = null;
 };
 
-mapModel.validateLatLng = function() {
+MapModel.prototype.validateLatLng = function() {
   return util.isNumber($("#lat").val()) && util.isNumber($("#lng").val());
 };
