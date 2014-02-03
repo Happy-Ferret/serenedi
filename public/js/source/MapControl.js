6560926f51508fc8f38f168d38516c367d3efefd
CodingTwinky <twinky@codingtwinky.com>
2014-02-02 22:05:27 -0600

    Using single quotes

--- public/js/source/MapControl.js
+++ public/js/source/MapControl.js
@@ -1,7 +1,7 @@
-var $ = require("../../../bower_components/jquery/jquery.min.js");
-var util = require("./Util.js");
-var statusObservable = require("./StatusObservable.js");
-var mapModel = require("./MapModel.js").mapModel;
+var $ = require('../../../bower_components/jquery/jquery.min.js');
+var util = require('./Util.js');
+var statusObservable = require('./StatusObservable.js');
+var mapModel = require('./MapModel.js').mapModel;
 
 var MapControl = can.Control({
   init: function(element, options) {
@@ -15,16 +15,16 @@ var MapControl = can.Control({
       callUpdateMap();
     }
   },
-  ".type change": function(el, ev) {
+  '.type change': function(el, ev) {
     typeChanged();
     mapModel.clearMap();
     callUpdateMap();
   },
-  ".datePicker change": function(el, ev) {
+  '.datePicker change': function(el, ev) {
     mapModel.clearMap();
     callUpdateMap();
   },
-  ".location change": function(el, ev) {
+  '.location change': function(el, ev) {
     var elementId = el.attr('id');
 
     if (elementId === 'lat') {
@@ -36,7 +36,7 @@ var MapControl = can.Control({
     mapModel.centerToLatLng();
     callUpdateMap();
   },
-  "#loadMyLocation click": function(el, ev) {
+  '#loadMyLocation click': function(el, ev) {
     loadMyLocation();
   }
 });
@@ -58,44 +58,44 @@ var loadMyLocation = function() {
 };
 
 var initializeMainElements = function(element) {
-  element.html(can.view("mapTemplate", {}));
+  element.html(can.view('mapTemplate', {}));
   mapModel.eventToOpenID = parseInt(util.getURLArgument.id, 10);
 
-  $("#dateFrom").datepicker({
-    defaultDate : "",
+  $('#dateFrom').datepicker({
+    defaultDate : '',
     changeMonth : true,
     changeYear : true,
     numberOfMonths : 1,
     onSelect : function(selectedDate) {
-      $("#dateTo").datepicker("option", "minDate", selectedDate);
-      $(this).trigger("change");
+      $('#dateTo').datepicker('option', 'minDate', selectedDate);
+      $(this).trigger('change');
     }
   });
-  $("#dateTo").datepicker({
-    defaultDate : "+1w",
+  $('#dateTo').datepicker({
+    defaultDate : '+1w',
     changeMonth : true,
     changeYear : true,
     numberOfMonths : 1,
     onSelect : function(selectedDate) {
-      $("#dateFrom").datepicker("option", "maxDate", selectedDate);
-      $(this).trigger("change");
+      $('#dateFrom').datepicker('option', 'maxDate', selectedDate);
+      $(this).trigger('change');
     }
   });
 
   var today = new Date();
-  $("#dateFrom").val(util.getPrettyDate(today));
-  $("#dateTo").datepicker("option", "minDate", today);
+  $('#dateFrom').val(util.getPrettyDate(today));
+  $('#dateTo').datepicker('option', 'minDate', today);
 
   var todayPlusOne = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7);
-  $("#dateTo").val(util.getPrettyDate(todayPlusOne));
-  $("#dateFrom").datepicker("option", "maxDate", todayPlusOne);
-  $("#sideMenu").mCustomScrollbar();
+  $('#dateTo').val(util.getPrettyDate(todayPlusOne));
+  $('#dateFrom').datepicker('option', 'maxDate', todayPlusOne);
+  $('#sideMenu').mCustomScrollbar();
 
-  $("#loadMyLocation").popover();
+  $('#loadMyLocation').popover();
 };
 
 var initializeMap = function () {
-  mapModel.map = new google.maps.Map(document.getElementById("mapBox"), {
+  mapModel.map = new google.maps.Map(document.getElementById('mapBox'), {
     zoom : 15,
     center : new google.maps.LatLng(mapModel.prop.lat, mapModel.prop.lng),
     mapTypeId : google.maps.MapTypeId.ROADMAP,
@@ -103,32 +103,32 @@ var initializeMap = function () {
     mapTypeControl: true
   });
 
-  google.maps.event.addListenerOnce(mapModel.map, "idle", function() {
+  google.maps.event.addListenerOnce(mapModel.map, 'idle', function() {
     var ne = mapModel.map.getBounds().getNorthEast();
     var sw = mapModel.map.getBounds().getSouthWest();
 
-    $("#radius").val(util.getDistanceFromLatLng(ne.lat(), ne.lng(), sw.lat(), sw.lng()) / 3);
+    $('#radius').val(util.getDistanceFromLatLng(ne.lat(), ne.lng(), sw.lat(), sw.lng()) / 3);
 
     mapModel.prop.attr('lat', util.roundNumber(mapModel.map.getCenter().lat()));
     mapModel.prop.attr('lng', util.roundNumber(mapModel.map.getCenter().lng()));
   });
 
-  google.maps.event.addListener(mapModel.map, "dragstart", function() {
+  google.maps.event.addListener(mapModel.map, 'dragstart', function() {
     mapModel.dragging = true;
   });
 
-  google.maps.event.addListener(mapModel.map, "dragend", function() {
+  google.maps.event.addListener(mapModel.map, 'dragend', function() {
     mapModel.prop.attr('lat', util.roundNumber(mapModel.map.getCenter().lat()));
     mapModel.prop.attr('lng', util.roundNumber(mapModel.map.getCenter().lng()));
     mapModel.dragging = false;
     callUpdateMap();
   });
 
-  google.maps.event.addListener(mapModel.map, "zoom_changed", function() {
+  google.maps.event.addListener(mapModel.map, 'zoom_changed', function() {
     var ne = mapModel.map.getBounds().getNorthEast();
     var sw = mapModel.map.getBounds().getSouthWest();
 
-    $("#radius").val(util.getDistanceFromLatLng(ne.lat(), ne.lng(), sw.lat(), sw.lng()));
+    $('#radius').val(util.getDistanceFromLatLng(ne.lat(), ne.lng(), sw.lat(), sw.lng()));
 
     callUpdateMap();
   });
@@ -136,16 +136,16 @@ var initializeMap = function () {
 
 var setupSocket = function() {
   var socketOptions = {
-    "transports" : [ "jsonp-polling" ],
-    "try multiple transports" : false,
-    "reconnect" : true,
-    "connect timeout" : 5000,
-    "reconnection limit attempts": 15
+    'transports' : [ 'jsonp-polling' ],
+    'try multiple transports' : false,
+    'reconnect' : true,
+    'connect timeout' : 5000,
+    'reconnection limit attempts': 15
   };
 
   mapModel.socket = io.connect(URL, socketOptions);
 
-  mapModel.socket.on("getEventsResult", function(data) {
+  mapModel.socket.on('getEventsResult', function(data) {
 
     if (data.message !== null) {
       if (data.center) {
@@ -153,10 +153,10 @@ var setupSocket = function() {
       }
 
       if (data.date) {
-        $("#dateFrom").datepicker("option", "maxDate", data.date.endDate);
-        $("#dateFrom").val(data.date.startDate);
-        $("#dateTo").datepicker("option", "minDate", data.date.startDate);
-        $("#dateTo").val(data.date.endDate);
+        $('#dateFrom').datepicker('option', 'maxDate', data.date.endDate);
+        $('#dateFrom').val(data.date.startDate);
+        $('#dateTo').datepicker('option', 'minDate', data.date.startDate);
+        $('#dateTo').val(data.date.endDate);
       }
 
       for (var n = 1; n < data.message.events.length; n++) {
@@ -168,9 +168,9 @@ var setupSocket = function() {
         }
       }
 
-      statusObservable.status.attr("value", 0);
+      statusObservable.status.attr('value', 0);
     } else {
-      statusObservable.status.attr("value", 2);
+      statusObservable.status.attr('value', 2);
     }
   });
 };
@@ -180,18 +180,18 @@ var isNeedUpdate = function() {
     return false;
   }
   // Is it current working?
-  if (statusObservable.status.attr("value") === 1) {
+  if (statusObservable.status.attr('value') === 1) {
     return false;
   }
-  if ($("#radius").val() > 19) {
-    statusObservable.status.attr("value", 3);
+  if ($('#radius').val() > 19) {
+    statusObservable.status.attr('value', 3);
     return false;
   } 
   if (!mapModel.validateLatLng()) {
     return false;
   }
 
-  return mapModel.distCheckPass || Math.abs(mapModel.getScreenTravelDistance()) > $("#radius").val() / 1.5;
+  return mapModel.distCheckPass || Math.abs(mapModel.getScreenTravelDistance()) > $('#radius').val() / 1.5;
 };
 
 var callUpdateMap = function () {
@@ -201,24 +201,24 @@ var callUpdateMap = function () {
 
 var updateMap = function() {
   if (isNeedUpdate()) {
-    statusObservable.status.attr("value", 1);
+    statusObservable.status.attr('value', 1);
     mapModel.latestLoc.lat = mapModel.prop.lat;
     mapModel.latestLoc.lng = mapModel.prop.lng;
 
     if (mapModel.eventToOpenID) {
-      mapModel.socket.emit("getEventsByIDCall", {
+      mapModel.socket.emit('getEventsByIDCall', {
         message: { id : mapModel.eventToOpenID,
-          radius : $("#radius").val()}
+          radius : $('#radius').val()}
         });
     } else {
-      mapModel.socket.emit("getEventsCall", {
+      mapModel.socket.emit('getEventsCall', {
         message: { 
           lat : mapModel.prop.lat,
           lng : mapModel.prop.lng,
-          dateFrom : $("#dateFrom").val(),
-          dateTo : $("#dateTo").val(),
-          type : $("#categories").val(),
-          radius : $("#radius").val() 
+          dateFrom : $('#dateFrom').val(),
+          dateTo : $('#dateTo').val(),
+          type : $('#categories').val(),
+          radius : $('#radius').val() 
         }
       });
     }
@@ -240,19 +240,19 @@ var addMarkers = function (event) {
 
   google.maps.event.addListener(
     marker,
-    "click",
+    'click',
     function() {
       mapModel.closeLastOpen();
 
       var info = new google.maps.InfoWindow({
-        content: can.view.render("infoPopUpTemplate",
+        content: can.view.render('infoPopUpTemplate',
         {   
           title: marker.getTitle(), 
-          url: {eventbrite: event.url, serenedi: URL + "/?id=" + event.id},
-          start: event.start_date.split(" ")[0],
-          end: event.end_date.split(" ")[0],
-          showAddr: event.venue.address !== null || event.venue.address !== "",
-          addr: event.venue.address + " " + event.venue.address_2,
+          url: {eventbrite: event.url, serenedi: URL + '/?id=' + event.id},
+          start: event.start_date.split(' ')[0],
+          end: event.end_date.split(' ')[0],
+          showAddr: event.venue.address !== null || event.venue.address !== '',
+          addr: event.venue.address + ' ' + event.venue.address_2,
           city: event.venue.city,
           region: event.venue.region,
           zip: event.venue.postalcode,
@@ -260,7 +260,7 @@ var addMarkers = function (event) {
         })
       });
 
-      google.maps.event.addListenerOnce(info, "closeclick", function() {
+      google.maps.event.addListenerOnce(info, 'closeclick', function() {
         marker.setAnimation(null);
       });
 
@@ -279,7 +279,7 @@ var addMarkers = function (event) {
     });
 
   if (event.id === mapModel.eventToOpenID) {
-    google.maps.event.trigger(marker, "click");
+    google.maps.event.trigger(marker, 'click');
     mapModel.eventToOpenID = null;
     var center = mapModel.map.getCenter();
     mapModel.prop.attr('lat', util.roundNumber(center.lat()));
@@ -288,34 +288,34 @@ var addMarkers = function (event) {
 };
 
 var flagCheck = function(element) {
-  if ($(element).prop("checked")) {
-    return "1";
+  if ($(element).prop('checked')) {
+    return '1';
   } else {
-    return "0";
+    return '0';
   }
 };
 
 var typeChanged = function() {
-  var result = flagCheck("#typeConfFlag");
+  var result = flagCheck('#typeConfFlag');
 
-  result += flagCheck("#typeConvFlag");
-  result += flagCheck("#typeEntFlag");
-  result += flagCheck("#typeFairFlag");
-  result += flagCheck("#typeFoodFlag");
-  result += flagCheck("#typeFundFlag");
-  result += flagCheck("#typeMeetFlag");
-  result += flagCheck("#typeMusicFlag");
-  result += flagCheck("#typePerfFlag");
-  result += flagCheck("#typeRecFlag");
-  result += flagCheck("#typeReligFlag");
-  result += flagCheck("#typeReunFlag");
-  result += flagCheck("#typeSalesFlag");
-  result += flagCheck("#typeSemiFlag");
-  result += flagCheck("#typeSociFlag");
-  result += flagCheck("#typeSportsFlag");
-  result += flagCheck("#typeTradeFlag");
-  result += flagCheck("#typeTravelFlag");
-  result += flagCheck("#typeOtherFlag");
+  result += flagCheck('#typeConvFlag');
+  result += flagCheck('#typeEntFlag');
+  result += flagCheck('#typeFairFlag');
+  result += flagCheck('#typeFoodFlag');
+  result += flagCheck('#typeFundFlag');
+  result += flagCheck('#typeMeetFlag');
+  result += flagCheck('#typeMusicFlag');
+  result += flagCheck('#typePerfFlag');
+  result += flagCheck('#typeRecFlag');
+  result += flagCheck('#typeReligFlag');
+  result += flagCheck('#typeReunFlag');
+  result += flagCheck('#typeSalesFlag');
+  result += flagCheck('#typeSemiFlag');
+  result += flagCheck('#typeSociFlag');
+  result += flagCheck('#typeSportsFlag');
+  result += flagCheck('#typeTradeFlag');
+  result += flagCheck('#typeTravelFlag');
+  result += flagCheck('#typeOtherFlag');
 
-  $("#categories").val(result);
+  $('#categories').val(result);
 };
--- public/js/source/StatusObservable.js
+++ public/js/source/StatusObservable.js
@@ -1,4 +1,4 @@
-var $ = require("../../../bower_components/jquery/jquery.min.js");
+var $ = require('../../../bower_components/jquery/jquery.min.js');
 
 var status = new can.Observe({value: 0});
 exports.status = status;
@@ -7,7 +7,7 @@ exports.status = status;
 // 1 working
 // 2 no events
 // 3 radius check fail
-status.bind("change", function(event, attr, how, newVal, oldVal) {
+status.bind('change', function(event, attr, how, newVal, oldVal) {
   switch (newVal) {
     case 0: 
       showNormal();
@@ -25,17 +25,17 @@ status.bind("change", function(event, attr, how, newVal, oldVal) {
 });
 
 var showWorking = function () {
-  $("#statusImg").attr({"data-content": "Working...", src: "/images/ajax-loader.gif"});
+  $('#statusImg').attr({'data-content': 'Working...', src: '/images/ajax-loader.gif'});
 };
 
 var showZoomCheckFail = function () {
-  $("#statusImg").attr({"data-content": "Zoom level is too high. Please zoom in to load events.", src: "/images/warning.png"});
+  $('#statusImg').attr({'data-content': 'Zoom level is too high. Please zoom in to load events.', src: '/images/warning.png'});
 };
 
 var showNoEvents = function () {
-  $("#statusImg").attr({"data-content": "There are no events with given criterias.", src: "/images/warning.png"});
+  $('#statusImg').attr({'data-content': 'There are no events with given criterias.', src: '/images/warning.png'});
 };
 
 var showNormal = function () {
-  $("#statusImg").attr({"data-content": "Welcome to Serenedi!", src: "/images/serenedi3.ico"});
+  $('#statusImg').attr({'data-content': 'Welcome to Serenedi!', src: '/images/serenedi3.ico'});
 };
