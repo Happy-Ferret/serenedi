7acff8c54f3908ce3ae8c7bd9adb2f596eda182e
CodingTwinky <twinky@codingtwinky.com>
2014-02-08 14:29:58 -0600

    None of the codes were necessary...  :P

--- public/js/source/MenuControl.js
+++ public/js/source/MenuControl.js
@@ -1,9 +1,7 @@
-var $ = require("../../../bower_components/jquery/jquery.min.js");
 var statusObservable = require('./StatusObservable.js');
 
 exports.MenuControl = can.Control({
   init: function(element, statusObservableOption) {
-    this.element.html(can.view("menuTemplate", statusObservableOption));
-    $("#statusImg").popover();
+    element.html(can.view("menuTemplate", statusObservableOption));
   }
 });
