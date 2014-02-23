c681f2b5047f3453bf3571aa8215cb7fe2a9ef35
CodingTwinky <twinky@codingtwinky.com>
2014-02-22 19:09:51 -0600

    Fixing status variable break.

--- public/js/source/StatusObservable.js
+++ public/js/source/StatusObservable.js
@@ -6,27 +6,27 @@ exports.status = new can.Observe({value: 0, content: 'Working...', src: '/images
 // 2 no events
 // 3 radius check fail
 // 4 geo location fail
-status.bind('change', function(event, attr, how, newVal, oldVal) {
+exports.status.bind('change', function(event, attr, how, newVal, oldVal) {
   switch (newVal) {
     case 0: 
-      status.attr('content', 'Welcome to Serenedi!');
-      status.attr('src', '/images/serenedi3.ico');
+      exports.status.attr('content', 'Welcome to Serenedi!');
+      exports.status.attr('src', '/images/serenedi3.ico');
       break;
     case 1:
-      status.attr('content', 'Working...');
-      status.attr('src', '/images/ajax-loader.gif');
+      exports.status.attr('content', 'Working...');
+      exports.status.attr('src', '/images/ajax-loader.gif');
       break;
     case 2:
-      status.attr('content', 'There are no events with given criterias.');
-      status.attr('src', '/images/warning.png');
+      exports.status.attr('content', 'There are no events with given criterias.');
+      exports.status.attr('src', '/images/warning.png');
       break;
     case 3:
-      status.attr('content', 'Zoom level is too high. Please zoom in to load events.');
-      status.attr('src', '/images/warning.png');
+      exports.status.attr('content', 'Zoom level is too high. Please zoom in to load events.');
+      exports.status.attr('src', '/images/warning.png');
       break;
     case 4:
-      status.attr('content', 'Geo location attrival is not avaliable for this browser.');
-      status.attr('src', '/images/warning.png');
+      exports.status.attr('content', 'Geo location attrival is not avaliable for this browser.');
+      exports.status.attr('src', '/images/warning.png');
       break;
   }
 });
