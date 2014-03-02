d16fc3604eba902b09625d272fe8551dcff52521
CodingTwinky <twinky@codingtwinky.com>
2014-03-02 16:02:19 -0600

    Grunt spacing fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,113 +7,113 @@ var config = require("./source/Config").config;
 module.exports = function(grunt) {
   var packageJson = grunt.file.readJSON('package.json');
   grunt.initConfig({
-      pkg: packageJson,
-      jade: {
-          compile: {
-              options: {
-                  data: { googleAPIKey: config.googleAPIKey, url: config.url },
-                  pretty: false
-              },
-              files: [ { 
-                expand: true, 
-                src: ["*.jade"], 
-                dest: "public/template", 
-                cwd: "views/", 
-                ext: '.html'
-              } ]
-          }
+    pkg: packageJson,
+    jade: {
+        compile: {
+            options: {
+                data: { googleAPIKey: config.googleAPIKey, url: config.url },
+                pretty: false
+            },
+            files: [ { 
+              expand: true, 
+              src: ["*.jade"], 
+              dest: "public/template", 
+              cwd: "views/", 
+              ext: '.html'
+            } ]
+        }
+    },
+    jshint: {
+      options: {
+        undef: true, // check for usage of undefined variables
+        '-W069': true, // ignore ['HEAD'] is better written in dot notation        
+        '-W079': true
       },
-      jshint: {
+      web: {
         options: {
-          undef: true, // check for usage of undefined variables
-          '-W069': true, // ignore ['HEAD'] is better written in dot notation        
-          '-W079': true
-        },
-        web: {
-          options: {
-            node: true,
-            browser: true,
-            globals: {
-              'google': true,
-              'FB': true,
-              'can': true,
-              'SERENEDI_URL': true
-            }
-          },
-          src: ['public/js/main.js', 'public/js/source/*', './*.js', 'source/*.js']
-        },
-        node: {
-          options: {
-            node: true
-          },
-          src: [
-            'source/*.js',
-            './*.js',
-          ]
-        },
-        phantomjs: {
-          options: {
-            phantom: true,
-            browser: true,
-            globals: {
-              '$': true,
-            }
-          },
-          src: ['clicktests/clicktests.js', 'configuredHelpers.js']
-        },
-        mocha: {
-          options: {
-            node: true,
-            globals: {
-              'it': true,
-              'describe': true,
-              'before': true,
-              'after': true,
-              'window': true,
-              'document': true,
-              'navigator': true
-            }
-          } 
-        }
-      },
-      less: {
-        production: {
-          files: {
-            "public/styles.css": ['public/css/styles.less', 'public/css/jquery.mCustomScrollbar.css', 'public/css/Serenedi-Theme/jquery-ui-1.10.3.serenedi.min.css']
+          node: true,
+          browser: true,
+          globals: {
+            'google': true,
+            'FB': true,
+            'can': true,
+            'SERENEDI_URL': true
           }
-        }
+        },
+        src: ['public/js/main.js', 'public/js/source/*', './*.js', 'source/*.js']
       },
-      browserify: {
-        dist: {
-          files: {
-            'public/main.js': ['public/js/main.js']
+      node: {
+        options: {
+          node: true
+        },
+        src: [
+          'source/*.js',
+          './*.js',
+        ]
+      },
+      phantomjs: {
+        options: {
+          phantom: true,
+          browser: true,
+          globals: {
+            '$': true,
           }
-        }
-      },
-      templates: {
-        files: ['public/*.html'],
-        tasks: ['templates'],
-        options: {
-          spawn: false,
         },
+        src: ['clicktests/clicktests.js', 'configuredHelpers.js']
       },
-      clean: ["public/template", "public/fonts", "public/jquery.min.map", "public/*.css", "public/*.js"],
-      copy: {
-        main: {
-          files: [
-            // includes files within path and its sub-directories
-            {expand: true, flatten: true, filter: 'isFile', src: 'bower_components/bootstrap/dist/fonts/*', dest: 'public/fonts/'},
-            {expand: true, flatten: true, src: 'bower_components/jquery/jquery.min.map', dest: 'public/'}
-          ]
-        }
-      },
-      simplemocha: {
+      mocha: {
         options: {
-          reporter: 'spec'
-        },
-
-        all: { src: 'test/*.js' }
+          node: true,
+          globals: {
+            'it': true,
+            'describe': true,
+            'before': true,
+            'after': true,
+            'window': true,
+            'document': true,
+            'navigator': true
+          }
+        } 
       }
+    },
+    less: {
+      production: {
+        files: {
+          "public/styles.css": ['public/css/styles.less', 'public/css/jquery.mCustomScrollbar.css', 'public/css/Serenedi-Theme/jquery-ui-1.10.3.serenedi.min.css']
+        }
+      }
+    },
+    browserify: {
+      dist: {
+        files: {
+          'public/main.js': ['public/js/main.js']
+        }
+      }
+    },
+    templates: {
+      files: ['public/*.html'],
+      tasks: ['templates'],
+      options: {
+        spawn: false,
+      },
+    },
+    clean: ["public/template", "public/fonts", "public/jquery.min.map", "public/*.css", "public/*.js"],
+    copy: {
+      main: {
+        files: [
+          // includes files within path and its sub-directories
+          {expand: true, flatten: true, filter: 'isFile', src: 'bower_components/bootstrap/dist/fonts/*', dest: 'public/fonts/'},
+          {expand: true, flatten: true, src: 'bower_components/jquery/jquery.min.map', dest: 'public/'}
+        ]
+      }
+    },
+    simplemocha: {
+      options: {
+        reporter: 'spec'
+      },
+
+      all: { src: 'test/*.js' }
+    }
   });
 
   var templateIncludeRegexp = /<!-- serenedi-import-[a-z]*Template: "([^"^.])*.html"-->/gm;
