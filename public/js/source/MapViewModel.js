395c4a7265eca35896e49f3de3faad5d0af2aec5
CodingTwinky <twinky@codingtwinky.com>
2014-06-04 08:11:49 -0500

    Moving types to side menu vm

--- public/js/source/MapViewModel.js
+++ public/js/source/MapViewModel.js
@@ -18,26 +18,6 @@ var MapViewModel = function() {
                                                 dateFrom: util.getPrettyDate(today),
                                                 dateTo: util.getPrettyDate(weekAfter)});
 
-  this.types = new can.Observe({conf: true,
-                                                conv: true,
-                                                ent: true,
-                                                fair: true,
-                                                food: true,
-                                                fund: true,
-                                                meet: true,
-                                                music: true,
-                                                perf: true,
-                                                rec: true,
-                                                relig: true,
-                                                reun: true,
-                                                sales: true,
-                                                semi: true,
-                                                soci: true,
-                                                sports: true,
-                                                trade: true,
-                                                travel: true,
-                                                other: true});
-
   this.map = null;
   this.ids = [];
   this.markers = [];
@@ -74,28 +54,6 @@ var MapViewModel = function() {
   this.prop.bind('types', function(event, newVal, oldVal) {
     self.clearMap();
     self.distCheckPass = true;
-  });
-
-  this.types.bind('change', function(event, attr, how, newVal, oldVal) {
-    self.prop.attr('types', (this.conf ? '1' : '0') +
-                                      (this.conv ? '1' : '0') +
-                                      (this.ent ? '1' : '0') +
-                                      (this.fair ? '1' : '0') +
-                                      (this.food ? '1' : '0') +
-                                      (this.fund ? '1' : '0') +
-                                      (this.meet ? '1' : '0') +
-                                      (this.music? '1' : '0') +
-                                      (this.perf ? '1' : '0') +
-                                      (this.rec ? '1' : '0') +
-                                      (this.relig ? '1' : '0') +
-                                      (this.reun ? '1' : '0') +
-                                      (this.sales ? '1' : '0') +
-                                      (this.semi ? '1' : '0') +
-                                      (this.soci ? '1' : '0') +
-                                      (this.sports ? '1' : '0') +
-                                      (this.trade ? '1' : '0') +
-                                      (this.travel ? '1' : '0') +
-                                      (this.other ? '1' : '0'));
   });
 
   this.map = new google.maps.Map(document.getElementById(MAP_BOX), {
--- public/js/source/SideMenuViewModel.js
+++ public/js/source/SideMenuViewModel.js
@@ -4,10 +4,54 @@ var mapVM = require('./MapViewModel.js').getMapViewModel();
 var sideMenuViewModel;
 
 var SideMenuViewModel = function() {
-  $('#sideMenu').html(can.view(SIDE_MENU_TEMPLATE, mapVM));
-
+  this.types = new can.Observe({conf: true,
+                                                conv: true,
+                                                ent: true,
+                                                fair: true,
+                                                food: true,
+                                                fund: true,
+                                                meet: true,
+                                                music: true,
+                                                perf: true,
+                                                rec: true,
+                                                relig: true,
+                                                reun: true,
+                                                sales: true,
+                                                semi: true,
+                                                soci: true,
+                                                sports: true,
+                                                trade: true,
+                                                travel: true,
+                                                other: true});
   this.dateFromDom = $('#dateFrom');
   this.dateToDom = $('#dateTo');
+
+  mapVM.types = this.types;
+
+  $('#sideMenu').html(can.view(SIDE_MENU_TEMPLATE, mapVM));
+  var self = this;
+
+  this.types.bind('change', function(event, attr, how, newVal, oldVal) {
+    self.prop.attr('types', (this.conf ? '1' : '0') +
+                                      (this.conv ? '1' : '0') +
+                                      (this.ent ? '1' : '0') +
+                                      (this.fair ? '1' : '0') +
+                                      (this.food ? '1' : '0') +
+                                      (this.fund ? '1' : '0') +
+                                      (this.meet ? '1' : '0') +
+                                      (this.music? '1' : '0') +
+                                      (this.perf ? '1' : '0') +
+                                      (this.rec ? '1' : '0') +
+                                      (this.relig ? '1' : '0') +
+                                      (this.reun ? '1' : '0') +
+                                      (this.sales ? '1' : '0') +
+                                      (this.semi ? '1' : '0') +
+                                      (this.soci ? '1' : '0') +
+                                      (this.sports ? '1' : '0') +
+                                      (this.trade ? '1' : '0') +
+                                      (this.travel ? '1' : '0') +
+                                      (this.other ? '1' : '0'));
+  });
 
   this.dateFromDom.datepicker({
     defaultDate : mapVM.prop.dateFrom,
@@ -20,6 +64,7 @@ var SideMenuViewModel = function() {
     },
     maxDate: mapVM.prop.dateTo
   });
+
   this.dateToDom.datepicker({
     defaultDate : mapVM.prop.dateFrom,
     changeMonth : true,
