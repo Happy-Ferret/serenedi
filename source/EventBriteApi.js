b79952cfe71fb5f05c078e5d478f564da765fa9c
CodingTwinky <twinky@codingtwinky.com>
2014-06-22 20:14:21 -0700

    Further extracting eventbriteAPI object

--- source/EventBriteApi.js
+++ source/EventBriteApi.js
@@ -1,11 +1,51 @@
-
 var Q = require('q');
 var argv = require('optimist').argv;
+var util = require('../shared/Util.js');
 var eventbrite = require("eventbrite");
 var eb_client = eventbrite({"app_key" : argv.eventbriteKey});
 
+var READ_SIZE = 100;
 
-module.exports.callEventSearch = function(param) {
+module.exports.searchEvents = function(req, res) {
+  callEventSearch(buildEventSearchParam(req.query)).then(function (data) {
+    console.log('[LOG] respose \n', data);
+    res.json(data);
+  }).fail(function (err) {
+    console.log('[ERROR] eventbrite search failed. \n', err, req.query);
+    res.json({'error': err});
+  });
+};
+
+module.exports.getEvent = function(req, res) {
+  var lat, lng, startDate, endDate;
+
+  callEventGet({'id': req.query.id, 'radius': Math.ceil(req.query.radius)}).then(function (data) {
+    lat = data.event.venue.latitude;
+    lng = data.event.venue.longitude;
+
+    var eventStartDate = new Date(data.event.start_date.split(" ")[0].split("-"));
+
+    startDate = util.getPrettyDate(eventStartDate);
+    eventStartDate.setDate(eventStartDate.getDate() + 7);
+    endDate = util.getPrettyDate(eventStartDate);
+
+    return buildEventSearchParam({'lat': lat, 'lng': lng, 'radius': req.query.radius, 'dateFrom': startDate, 'dateTo': endDate, 'type': null});
+  }).then(callEventSearch)
+  .then(function (data) {
+    data.center = {'lat': lat, 'lng': lng};
+    data.date = {'startDate': startDate, 'endDate': endDate};
+
+    console.log('[LOG] respose \n', data);
+
+    res.json(data);
+  }).fail(function (err) {
+    console.log('[ERROR] get event by id failed. \n', err, req.query);
+
+    res.json({'error': err});
+  });
+};
+
+var callEventSearch = function(param) {
   console.log('[LOG] search events\n', param);
 
   var deferred = Q.defer();
@@ -20,7 +60,7 @@ module.exports.callEventSearch = function(param) {
   return deferred.promise;
 };
 
-module.exports.callEventGet = function(param) {
+var callEventGet = function(param) {
   console.log('[LOG] get events\n', param);
 
   var deferred = Q.defer();
@@ -33,4 +73,18 @@ module.exports.callEventGet = function(param) {
   });
 
   return deferred.promise;
+};
+
+var buildEventSearchParam = function(args) {
+  return {
+    'latitude': args.lat,
+    'longitude': args.lng,
+    "within_unit" : "K",
+    "max" : READ_SIZE,
+    "page" : 1,
+    "within" : Math.ceil(args.radius),
+    "date" : util.getEventbriteDateFormat(args.dateFrom) + " " + util.getEventbriteDateFormat(args.dateTo),
+    "category" : util.getTypeString(args.type),
+    "sort_by" : "id"
+  };
 };
--- source/Main.js
+++ source/Main.js
@@ -1,8 +1,5 @@
-var READ_SIZE = 100;
-
 var path = require("path");
 var express = require("express");
-var util = require('../shared/Util.js');
 var argv = require('optimist').argv;
 var eventBriteApi = require('./EventBriteApi.js');
 var app = express();
@@ -11,58 +8,13 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.get("/", function (req, res) { res.redirect("../index.html"); });
 
 app.get("/api/getEvents", function(req, res) {
-  eventBriteApi.callEventSearch(buildEventSearchParam(req.query)).then(function (data) {
-    console.log('[LOG] respose \n', data);
-    res.json(data);
-  }).fail(function (err) {
-    console.log('[ERROR] event search failed. \n', err, req.query);
-    res.json({'error': err});
-  });
+  eventBriteApi.searchEvents(req, res);
 });
 
 app.get("/api/getEventsById", function(req, res) {
-  var lat, lng, startDate, endDate;
-
-  eventBriteApi.callEventGet({'id': req.query.id, 'radius': Math.ceil(req.query.radius)}).then(function (data) {
-    lat = data.event.venue.latitude;
-    lng = data.event.venue.longitude;
-
-    var eventStartDate = new Date(data.event.start_date.split(" ")[0].split("-"));
-
-    startDate = util.getPrettyDate(eventStartDate);
-    eventStartDate.setDate(eventStartDate.getDate() + 7);
-    endDate = util.getPrettyDate(eventStartDate);
-
-    return buildEventSearchParam({'lat': lat, 'lng': lng, 'radius': req.query.radius, 'dateFrom': startDate, 'dateTo': endDate, 'type': null});
-  }).then(eventBriteApi.callEventSearch)
-  .then(function (data) {
-    data.center = {'lat': lat, 'lng': lng};
-    data.date = {'startDate': startDate, 'endDate': endDate};
-
-    console.log('[LOG] respose \n', data);
-
-    res.json(data);
-  }).fail(function (err) {
-    console.log('[ERROR] get event by id failed. \n', err, req.query);
-
-    res.json({'error': err});
-  });
+  eventBriteApi.getEvent(req, res);
   // getEventsById(req.query, res);
 });
-
-var buildEventSearchParam = function(args) {
-  return {
-    'latitude': args.lat,
-    'longitude': args.lng,
-    "within_unit" : "K",
-    "max" : READ_SIZE,
-    "page" : 1,
-    "within" : Math.ceil(args.radius),
-    "date" : util.getEventbriteDateFormat(args.dateFrom) + " " + util.getEventbriteDateFormat(args.dateTo),
-    "category" : util.getTypeString(args.type),
-    "sort_by" : "id"
-  };
-};
 
 app.listen(argv.port);
 console.log("## Serenedi started ##");
