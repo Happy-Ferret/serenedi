ddffeefb5c823325eda9d77b39aad846ad360a0c
codingtwinky <twinky@codingtwinky.com>
2014-06-27 23:47:24 -0700

    Fixing test

--- test/test.MeetUpApi.js
+++ test/test.MeetUpApi.js
@@ -7,8 +7,7 @@ describe('Testing MeetUp api', function () {
     var searchParam = meetUp.buildEventSearchParam({lng: 1, lat: 2, radius: 3, dateFrom: '06/24/2014', dateTo: '06/28/2014'});
 
     // Key will be 'undefined' for it is being passed in as an option argument.
-    expect(searchParam).to.eql({host:'https://api.meetup.com',port:'80',path:'/2/open_events.json?lon=1&lat=2&radius=1.86411&time=1403593200000,1403938800000&key=undefined',method:'GET'});
-
+    expect(searchParam).to.eql('http://api.meetup.com/2/open_events.json?lon=1&lat=2&radius=1.86411&time=1403593200000,1403938800000&key=undefined');
     done();
   });
 
